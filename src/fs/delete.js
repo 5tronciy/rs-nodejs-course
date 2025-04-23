2839ec57a6b270b97cceedf235eaf15eae6b6be9
5tronciy <github@5tronciy.org>
2025-04-23 23:30:48 +0200

    feat: add remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import { access, unlink } from 'node:fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await access(filePath);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await unlink(filePath);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
