531c2cbf4a2db973b603ddf3533d92bcbcf6530c
5tronciy <github@5tronciy.org>
2025-04-24 19:40:04 +0200

    feat: add streams:transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
+const reverseTransform = new Transform({
+    transform(chunk, _encoding, callback) {
+        const input = chunk.toString().trim();
+        const reversed = input.split('').reverse().join('');
+        this.push(reversed + '\n');
+        callback();
+    }
+});
+
 const transform = async () => {
-    // Write your code here 
+    try {
+        await pipeline(
+            process.stdin,
+            reverseTransform,
+            process.stdout
+        );
+    } catch {
+        throw new Error('Stream failed');
+    }
 };
 
 await transform();
