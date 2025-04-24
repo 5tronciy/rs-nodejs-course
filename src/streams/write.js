58409b2dfd02877a449fbd72465b795deeedef2c
5tronciy <github@5tronciy.org>
2025-04-24 19:32:56 +0200

    feat: add streams:write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,29 @@
+import { createWriteStream } from 'node:fs';
+import { access } from 'node:fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'node:stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+    const dirPath = join(__dirname, 'files');
+
+    try {
+      await access(dirPath);
+    } catch {
+      throw new Error('FS operation failed');
+    }
+  
+    try {
+      const stream = createWriteStream(filePath);
+
+      await pipeline(process.stdin, stream);
+    } catch {
+      throw new Error('Stream failed');
+    }
 };
 
 await write();
