6aa5f347f86f9259c74d467ecebfb58a6c842bb9
5tronciy <github@5tronciy.org>
2025-04-28 22:41:49 +0200

    feat: add worker thread with nthFibonacci calculations

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
+import { parentPort } from 'node:worker_threads';
+
+if (!parentPort) {
+    throw new Error('Not running inside a worker thread');
+}
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const sendResult = (n) => {
+    const result = nthFibonacci(n);
+    parentPort.postMessage(result);
 };
 
-sendResult();
+parentPort.once('message', (n) => {
+    sendResult(n);
+});
